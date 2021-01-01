9523e723fb484b1a4a5f98fa23286a5f44a11245
zerothehero555 <73942835+zerothehero555@users.noreply.github.com>
2021-01-01 18:58:18 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,24 @@
+class Block{
+     constructor(x, y, width, height) {
+          var options = { 
+              restitution :0.4, 
+              friction :0.0,
+               //isStatic:true 
+            } 
+            this.body = Bodies.rectangle(x, y, width, height, options); 
+            this.width = width;
+             this.height = height; 
+             World.add(world, this.body);
+             } 
+             display(){ 
+                 var angle = this.body.angle; 
+                 var pos= this.body.position;
+                  push(); 
+                  translate(pos.x, pos.y);
+                   rotate(angle); 
+                   fill("orange")
+                   rectMode(CENTER); 
+                   rect(0,0,this.width,this.height);
+                      pop();
+                     }
+                     }
